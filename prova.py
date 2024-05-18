a3a8aebac52c55e7aa987f5885ede993b2c7ff64
Nataly Jaya Salazar <Nataliesalazar195@gmail.com>
2024-05-18 14:38:22 +0200

    provaNatFet
    Inversas hechas menos las traducciones

---
+++ prova.py
@@ -0,0 +1,20 @@
+from deep_translator import GoogleTranslato
+
+
+#ESTE PROGRAMA LEE Y ECRIBE A LA INVERSA UNA PALABRA Y FRASE DE UN FICHERO
+"""
+if __name__ == '__main__':
+
+    f = open("input.txt", "r")
+    for x in f:
+        x = x.rstrip()
+        reversed_x = x[::-1]
+        print(reversed_x)
+
+
+
+   
+    data = "holis"
+    print(data[::-1])
+    """
+ #EL PROGRAMA TRADUCIRIA
