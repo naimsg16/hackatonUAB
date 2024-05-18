809678f804589e5cfbff40da70392d9756c3a401
naimsg <naimsaadigallego@gmail.com>
2024-05-18 15:34:14 +0200

    Comentaris complets

---
+++ prova.py
@@ -0,0 +1,8 @@
+import random
+print("hola") # això és una prova
+print()
+
+n = 3 # nombre
+while(n > 8):
+    n += 1
+    print('hello') # hole
--- reading.py
+++ reading.py
@@ -1,22 +1,25 @@
-def check_comment (line):
-        for token in line:
-            if( '#' in token):
-                content = token.split('#')
-                print(content)
-                code = content[0]
-                comment = content[1]
-                file2.write(f"{code} # {comment.upper()} \n")
+def check_comment (token):
+    if( '#' in token):
+        content = token.split('#')
+        code = content[0]
+        comment = content[1]
+        file2.write(f"{code}#{comment.upper()} \n")
+    elif token != '':
+        file2.write(token + '\n')
+    else:
+        file2.write('\n')
 
 
 if __name__=="__main__":            
-    with open("prova.py","r") as file:
+    with open("prova.py","r", encoding="utf-8") as file:
         lines = file.readlines()
     
-    with open("output.py","a") as file2:
+    with open("output.py","a", encoding="utf-8") as file2:
         file2.truncate(0)
         for line in lines:
             tokenized =  line.split('\n')
-            check_comment(tokenized)
+            print(tokenized[0])
+            check_comment(tokenized[0])
 
 
 
