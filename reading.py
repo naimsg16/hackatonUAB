ed9f8cddc43bf10a2b88c8c2723f18b348621c4d
naimsg <naimsaadigallego@gmail.com>
2024-05-18 15:05:16 +0200

    Canvia els comentaris sencers

--- reading.py
+++ reading.py
@@ -3,15 +3,16 @@ def check_comment (line):
             if( '#' in token):
                 content = token.split('#')
                 print(content)
+                code = content[0]
                 comment = content[1]
-                file2.write(comment + '\n')
+                file2.write(f"{code} # {comment.upper()} \n")
 
 
 if __name__=="__main__":            
     with open("prova.py","r") as file:
         lines = file.readlines()
     
-    with open("output.txt","a") as file2:
+    with open("output.py","a") as file2:
         file2.truncate(0)
         for line in lines:
             tokenized =  line.split('\n')
